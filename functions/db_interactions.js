24d4489c776cd02a6a73dc6d290e40b2a7b8c592
ulfsarkk <ulfsark@geode.fi>
2023-05-01 18:38:33 +0300

    Getcontacts GetProfile and updateStatus end points are created

--- functions/db_interactions.js
+++ functions/db_interactions.js
@@ -1,145 +1,197 @@
+/*  User Interactions   */
 
-
-export function AddUser(Pool, user, respond){
+export function AddUser(Pool, user, respond) {
     console.log(user);
 
     Pool.query(`INSERT INTO public.users(
         name, password, email, language, gender)
-        VALUES ('`+user.name+"','"+ user.password+"','"+ user.email+"','"+ user.language +"',"+ user.gender+")", 
-        (err, res)=>{
-            if(err){
+        VALUES ('`+ user.name + "','" + user.password + "','" + user.email + "','" + user.language + "'," + user.gender + ")",
+        (err, res) => {
+            if (err) {
                 console.log("Error!");
                 console.log(err)
-                respond.send({"success": false, "error": err.detail})
+                respond.send({ "success": false, "error": err.detail })
             }
             else {
-                respond.send({"success": true});
+                respond.send({ "success": true });
             }
-    })
+        })
 }
 
-export function UserLogin(Pool, user, respond){
+export function UserLogin(Pool, user, respond) {
     console.log(user);
 
     Pool.query(`SELECT id, name, password, email, language, gender
-	FROM public.users where email = '`+user.email+"';", 
-        (err, res)=>{
-            if(err){
+	FROM public.users where email = '`+ user.email + "';",
+        (err, res) => {
+            if (err) {
                 console.log("Error!");
                 console.log(err)
-                respond.send({"success": false, "error": err.detail})
+                respond.send({ "success": false, "error": err.detail })
             }
             else {
-                if(res.rows.length < 1){
+                if (res.rows.length < 1) {
                     console.log("No such a user!");
-                    respond.send({"success": false, "error": "No such a user!"});
+                    respond.send({ "success": false, "error": "No such a user!" });
                 }
-                else if(res.rows[0].password == user.password){
-                    respond.send({"success": true, "user": res.rows[0]});
+                else if (res.rows[0].password == user.password) {
+                    respond.send({ "success": true, "user": res.rows[0] });
                 }
                 else {
-                    respond.send({"success": false, "error": "Wrong password"});
+                    respond.send({ "success": false, "error": "Wrong password" });
                 }
-                
-                
+
+
             }
+        })
+}
+
+export function GetContactsSQL(Pool, user_id) {
+    return new Promise((resolve) => {
+        Pool.query(`SELECT id, name, language, status FROM public."users";`,
+            (err, res) => {
+                if (err) {
+                    console.log("Error cought when getting participant of meeting!")
+                    console.log(err);
+                    resolve({ "success": false, "error": err.detail })
+                }
+                else {
+                    resolve(res.rows);
+                }
+            })
     })
 }
 
+export function GetProfileSQL(Pool, user_id) {
+    return new Promise((resolve) => {
+        Pool.query(`SELECT id, name, email, gender, language, status FROM public."users" WHERE id=` + user_id,
+            (err, res) => {
+                if (err) {
+                    console.log("Error cought when getting participant of meeting!")
+                    console.log(err);
+                    resolve({ "success": false, "error": err.detail })
+                }
+                else {
+                    resolve(res.rows);
+                }
+            })
+    })
+}
+
+export function UpdateStatusSQL(Pool, user_id, status) {
+    return new Promise((resolve) => {
+        Pool.query(`UPDATE public.users SET status=${status} WHERE id=` + user_id,
+            (err, res) => {
+                if (err) {
+                    console.log("Error cought when getting participant of meeting!")
+                    console.log(err);
+                    resolve({ "success": false, "error": err.detail })
+                }
+                else {
+                    resolve({ "success": true });
+                }
+            })
+    })
+}
 
 // Meeting Interactions !!!
 
-export function GetParticipantsSQL(Pool, meeting_id){
-    return new Promise((resolve) => { Pool.query(`SELECT name, id, language, gender FROM public."MeetingUsers" JOIN public.users ON user_id = id WHERE meeting_id = `+meeting_id+`;`,
-        (err,res) => {
-            if(err){
-                console.log("Error cought when getting participant of meeting!")
-                console.log(err);
-                resolve({"success": false, "error": err.detail})
-            }
-            else {
-                resolve(res.rows);
-            }
-        })
-    })
-}
-
-
-export async function CreateMeetingSQL(Pool, name, user_id){
-    return new Promise((resolve) => { Pool.query(`INSERT INTO public.meetings(name, owner_id) VALUES ( '`+name+"', "+user_id+`);`,
-        (err,res) => {
-            if(err){
-                console.log("Error cought when creating meeting!")
-                if(err.constraint == 'owner_id_unq'){
-                    console.log("You have already an ongoing meeting!")
-                    resolve({success: true});
+export function GetParticipantsSQL(Pool, meeting_id) {
+    return new Promise((resolve) => {
+        Pool.query(`SELECT name, id, language, gender FROM public."MeetingUsers" JOIN public.users ON user_id = id WHERE meeting_id = ` + meeting_id + `;`,
+            (err, res) => {
+                if (err) {
+                    console.log("Error cought when getting participant of meeting!")
+                    console.log(err);
+                    resolve({ "success": false, "error": err.detail })
                 }
                 else {
-                    console.log(err);
-                    resolve({success: false, error: err.detail})
+                    resolve(res.rows);
                 }
-            }
-            else {
-                console.log("Meeting Created on Table!");
-                resolve({success: true});
-            }
-        })
+            })
     })
 }
 
 
-export async function GetMyMeetingSQL(Pool, user_id){
-    return new Promise((resolve) => { Pool.query(`SELECT id FROM public.meetings WHERE owner_id = `+user_id+`;`,
-        (err,res) => {
-            if(err){
-                console.log("Error cought when finding the meeting of the user!")
-                console.log(err);
-                resolve({success: false, "error": err.detail})
-            }
-            else {
-                console.log(res);
-                resolve({success: true, meeting: res.rows[0]});
-            }
-        })
-    })
-}
-
-
-export async function AddUserToMeetingSQL(Pool, user_id, meeting_id){
-
-    return new Promise((resolve) => { Pool.query(`INSERT INTO public."MeetingUsers"( user_id, meeting_id) VALUES (`+user_id+", "+meeting_id+`);`,
-        (err,res) => {
-            if(err){
-                console.log("Error cought when joining the meeting!")
-                if(err.constraint == "MeetingUsers_PK"){
-                    console.log("User already in the meeting");
+export async function CreateMeetingSQL(Pool, name, user_id) {
+    return new Promise((resolve) => {
+        Pool.query(`INSERT INTO public.meetings(name, owner_id) VALUES ( '` + name + "', " + user_id + `);`,
+            (err, res) => {
+                if (err) {
+                    console.log("Error cought when creating meeting!")
+                    if (err.constraint == 'owner_id_unq') {
+                        console.log("You have already an ongoing meeting!")
+                        resolve({ success: true });
+                    }
+                    else {
+                        console.log(err);
+                        resolve({ success: false, error: err.detail })
+                    }
+                }
+                else {
+                    console.log("Meeting Created on Table!");
                     resolve({ success: true });
                 }
-                else {
-                    console.log(err);
-                    resolve({success: false, error: err.detail})
-                }
-            }
-            else {
-                console.log(res);
-                resolve({ success: true });
-            }
-        })
+            })
     })
 }
 
-export async function DeleteUserFromMeetingSQL(Pool, user_id, meeting_id){
 
-    return new Promise((resolve) => { Pool.query(`DELETE FROM public."MeetingUsers" WHERE user_id = `+user_id+`  AND meeting_id = `+meeting_id+`;`,
-        (err,res) => {
-            if(err){
-                console.log("Error cought when leaving the meeting!")
-                console.log(err);
-                resolve({success: false, error: err.detail})
-            }
-            else {
-                resolve({ success: true });
-            }
-        })
+export async function GetMyMeetingSQL(Pool, user_id) {
+    return new Promise((resolve) => {
+        Pool.query(`SELECT id FROM public.meetings WHERE owner_id = ` + user_id + `;`,
+            (err, res) => {
+                if (err) {
+                    console.log("Error cought when finding the meeting of the user!")
+                    console.log(err);
+                    resolve({ success: false, "error": err.detail })
+                }
+                else {
+                    console.log(res);
+                    resolve({ success: true, meeting: res.rows[0] });
+                }
+            })
+    })
+}
+
+
+export async function AddUserToMeetingSQL(Pool, user_id, meeting_id) {
+
+    return new Promise((resolve) => {
+        Pool.query(`INSERT INTO public."MeetingUsers"( user_id, meeting_id) VALUES (` + user_id + ", " + meeting_id + `);`,
+            (err, res) => {
+                if (err) {
+                    console.log("Error cought when joining the meeting!")
+                    if (err.constraint == "MeetingUsers_PK") {
+                        console.log("User already in the meeting");
+                        resolve({ success: true });
+                    }
+                    else {
+                        console.log(err);
+                        resolve({ success: false, error: err.detail })
+                    }
+                }
+                else {
+                    console.log(res);
+                    resolve({ success: true });
+                }
+            })
+    })
+}
+
+export async function DeleteUserFromMeetingSQL(Pool, user_id, meeting_id) {
+
+    return new Promise((resolve) => {
+        Pool.query(`DELETE FROM public."MeetingUsers" WHERE user_id = ` + user_id + `  AND meeting_id = ` + meeting_id + `;`,
+            (err, res) => {
+                if (err) {
+                    console.log("Error cought when leaving the meeting!")
+                    console.log(err);
+                    resolve({ success: false, error: err.detail })
+                }
+                else {
+                    resolve({ success: true });
+                }
+            })
     })
 }
--- routes.js
+++ routes.js
@@ -11,8 +11,8 @@ const Speech = v1.TextToSpeechClient;
 
 const projectId = 'wired-compass-334608';
 
-const translate = new Translate({projectId});
-const speech = new Speech({projectId})
+const translate = new Translate({ projectId });
+const speech = new Speech({ projectId })
 
 /* Database Initilize */
 import pool from "./db_initialize.js"
@@ -20,8 +20,10 @@ import pool from "./db_initialize.js"
 const meetings = []
 
 async function Translater(req, res) {
+
   // The text to translate
   const text = await req.query.text;
+  console.log("text:", text);
 
   // source language
   const source = await req.query.sourcelang;
@@ -44,141 +46,182 @@ async function Translater(req, res) {
 
 
   const audio = await TextToSpeech(translation, target);
-  
+
   var u8 = new Uint8Array(audio[0].audioContent);
   var b64 = Buffer.from(u8).toString('base64')
-  
+
   audio[0].audioContent = b64;
-  res.send({translation, audio})
+  res.send({ translation, audio })
 }
 
 const router = Router();
 
 //import Transcripta from "./"
 
-router.post('/send_speech', SendSpeechHTTP);
+router.post('/send_speech', GetSpeech);
 router.post('/create_meeting', CreateMeeting);
 router.post('/join_meeting', JoinMeeting);
 router.post('/leave_meeting', LeaveMeeting);
 router.post('/change_language', ChangeLanguage);
 router.post('/sign_up', SignUp)
 router.post('/login', Login)
+router.post('/update_status', UpdateStatus);
 //router.post('/transcript', Transcript);
+router.get('/get_contacts', GetContacts);
+router.get('/get_profile', GetProfile);
 router.get('/translate', Translater);
-router.get('/texttospeech',TextToSpeechHTTP)
+router.get('/texttospeech', TextToSpeechHTTP);
 
 //router.ws("/meeting_hub", MeetingWS);
 
 // User Interactions !!!
 import { AddUser, UserLogin } from "./functions/db_interactions.js";
 
-export async function SignUp(req,res){
+export async function SignUp(req, res) {
   try {
     AddUser(pool, req.body.body, res);
-    
-  } catch(err) {
-    console.log("Error",err);
+
+  } catch (err) {
+    console.log("Error", err);
   }
 }
 
-export async function Login(req,res){
+export async function Login(req, res) {
   try {
     UserLogin(pool, req.body.body, res);
 
-  } catch(err) {
-    console.log("Error",err);
+  } catch (err) {
+    console.log("Error", err);
   }
 }
 
 
 // Google Api Methods !!!
 
-async function SendSpeechHTTP(req, res) {
-  try {
-    res.send({"result":"success"});
-  } catch(err) {
-    console.log("Error",err);
-  }
-}
 
 
 
 // Meeting Functions !!!
-import MeetingHub, { SendNewUser, CreateHub } from "./ws_hubs.js"
-import { GetParticipantsSQL, GetMyMeetingSQL, CreateMeetingSQL, AddUserToMeetingSQL, DeleteUserFromMeetingSQL } from "./functions/db_interactions.js"
+import { SendNewUser, CreateHub, SendSpeechToParticipants } from "./ws_hubs.js"
+import { GetContactsSQL, GetProfileSQL, UpdateStatusSQL, GetParticipantsSQL, GetMyMeetingSQL, CreateMeetingSQL, AddUserToMeetingSQL, DeleteUserFromMeetingSQL } from "./functions/db_interactions.js"
 
 
-async function CreateMeeting(req,res){
+async function CreateMeeting(req, res) {
   try {
     var response = await CreateMeetingSQL(pool, /* req.body.meeting_name */ "Meeting", req.body.owner_id);
-    if(response?.success){
+    if (response?.success) {
       var meeting = await GetMyMeetingSQL(pool, req.body.owner_id)
       AddUserToMeetingSQL(pool, req.body.owner_id, meeting.meeting.id);
       CreateHub(req.app.get('io'), req.body.owner_id, meeting.meeting.id);
-      res.send({success: true, meeting: meeting.meeting});
+      res.send({ success: true, meeting: meeting.meeting });
     }
     res.send(response);
-  } catch(err) {
-    console.log("Error",err);
+  } catch (err) {
+    console.log("Error", err);
   }
 }
-async function JoinMeeting(req,res){
+async function JoinMeeting(req, res) {
   try {
     var response = await AddUserToMeetingSQL(pool, req.body.user.id, req.body.meeting_id);
     console.log(response)
-    if(response?.success){
+    if (response?.success) {
       console.log("Succesfully joined!");
       var participants = await GetParticipantsSQL(pool, req.body.meeting_id);
-      participants.forEach((user)=>{
+      participants.forEach((user) => {
         console.log(user);
         SendNewUser(req.app.get('io'), req.body.meeting_id, user.id, participants);
       })
-      res.send({success: true, participants: participants});
+      res.send({ success: true, participants: participants });
     }
     else {
       res.send(response);
     }
-  } catch(err) {
-    console.log("Error",err);
+  } catch (err) {
+    console.log("Error", err);
   }
 }
-async function LeaveMeeting(req, res){
+async function LeaveMeeting(req, res) {
   try {
     var response = await DeleteUserFromMeetingSQL(pool, req.body.user_id, req.body.meeting_id);
-    if(response?.success){
+    if (response?.success) {
       console.log("Succesfully left!");
       var participants = await GetParticipantsSQL(pool, req.body.meeting_id);
-      participants.forEach((user)=>{
+      participants.forEach((user) => {
         console.log(user);
         SendNewUser(req.app.get('io'), req.body.meeting_id, user.id, participants);
       })
     }
     res.send(response)
-  } catch(err) {
-    console.log("Error",err);
+  } catch (err) {
+    console.log("Error", err);
   }
 }
 
-async function ChangeLanguage(req,res){
+async function ChangeLanguage(req, res) {
   try {
-    for(let i=0; i<meetings[0].users.length; i++){
-      if(meetings[0].users[i].name == req.body.user){
+    for (let i = 0; i < meetings[0].users.length; i++) {
+      if (meetings[0].users[i].name == req.body.user) {
         meetings[0].users[i].lang = req.body.lang
       }
-      console.log("User "+req.body.user+" changed his language to "+ req.body.lang);
+      console.log("User " + req.body.user + " changed his language to " + req.body.lang);
     }
     req.app.set('meetings', meetings);
     res.send("success");
-  } catch(err) {
-    console.log("Error",err);
+  } catch (err) {
+    console.log("Error", err);
   }
 }
 
+async function GetSpeech(req, res) {
+  try {
+    console.log(req.body)
+    var participants = await GetParticipantsSQL(pool, req.body.meeting_id);
+    participants.forEach((user) => {
+      console.log(user);
+      if (user.id != req.body.user_id) {
+        SendSpeechToParticipants(req.app.get('io'), req.body.meeting_id, user.id, req.body.user.id, req.body.text, req.body.user.lang);
+      }
+    })
+    res.send({ "result": "success" });
+  } catch (err) {
+    console.log("Error", err);
+  }
+}
+
+async function GetContacts(req, res) {
+  try {
+    console.log("GetContacts...");
+    var participants = await GetContactsSQL(pool, 1);
+    res.send(participants);
+  } catch (err) {
+    console.log("Error", err);
+  }
+}
+
+async function GetProfile(req, res) {
+  try {
+    console.log("Obtaining profile with id " + req.query.user_id + "...");
+    var profile = await GetProfileSQL(pool, req.query.user_id);
+    res.send(profile);
+  } catch (err) {
+    console.log("Error", err);
+  }
+}
+
+async function UpdateStatus(req, res) {
+  try {
+    console.log("Obtaining profile with id " + req.body.user_id + "...");
+    var result = await UpdateStatusSQL(pool, req.body.user_id, req.body.status);
+    res.send(result);
+  } catch (err) {
+    console.log("Error", err);
+  }
+}
 
 const request = {
-  input: { text: ""},
-  voice: {languageCode: 'tr-TR', ssmlGender: 'NEUTRAL'},
-  audioConfig: { audioEncoding: 'MP3'}
+  input: { text: "" },
+  voice: { languageCode: 'tr-TR', ssmlGender: 'NEUTRAL' },
+  audioConfig: { audioEncoding: 'MP3' }
 }
 
 async function TextToSpeechHTTP(req, res) {
@@ -187,24 +230,24 @@ async function TextToSpeechHTTP(req, res) {
   try {
     const response = await speech.synthesizeSpeech(request);
     res.send(response);
-  } catch(err) {
-    console.log("Error",err);
+  } catch (err) {
+    console.log("Error", err);
   }
 }
 
-async function TextToSpeech(text, lang){
+async function TextToSpeech(text, lang) {
   request.input.text = text;
   request.voice.languageCode = lang;
   try {
     const response = await speech.synthesizeSpeech(request);
     return response;
-  } catch(err) {
-    console.log("Error",err);
+  } catch (err) {
+    console.log("Error", err);
   }
 }
 
-function MeetingWS(ws, req){
-  ws.on('message', function(msg) {
+function MeetingWS(ws, req) {
+  ws.on('message', function (msg) {
     ws.send(msg);
   });
 }
